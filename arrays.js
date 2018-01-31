4b827b7d1e94d012e0eb43c105c3baa8f6037dbc
Kevin Li <kevinjcli@hotmail.com>
2018-01-31 23:28:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,25 +1,25 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element) {
-  array = [element, ...array]
-  return array
+  array = [element, ...array];
+  return array;
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray (array, element) {
-  array = [...array, element]
-  return array
+  array = [...array, element];
+  return array;
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray (array, index) {
-  return array[index]
+  return array[index];
 } 
